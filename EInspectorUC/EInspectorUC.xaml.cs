53e361dbf3db990b2a2fcde0cfab80d3f3cd23d1
John <jsu@orbisblue.com>
2013-04-08 20:35:18 -0400

    Attempting to get binding working in form region

--- EInspectorUC/EInspectorUC.xaml.cs
+++ EInspectorUC/EInspectorUC.xaml.cs
@@ -12,18 +12,35 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Office.Interop.Outlook;
+using System.ComponentModel;
 
 namespace EInspectorUC
 {
     /// <summary>
     /// Interaction logic for EInspectorUC.xaml
     /// </summary>
-    public partial class EInspectorUC : UserControl
+    public partial class EInspectorUC : UserControl, INotifyPropertyChanged
     {
+        private MailItem _mailItem;
+
+        public MailItem Mail
+        {
+            get { return _mailItem; }
+            set
+            {
+                _mailItem = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Mail"));
+            }
+        }
+
         public EInspectorUC()
         {
+            DataContext = this;
             InitializeComponent();
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         private void TestB_Click(object sender, RoutedEventArgs e)
         {
